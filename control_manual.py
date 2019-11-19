30c507d349bde4d9a59932ad1ce6607e2ae44648
dajun928 <1663177102@qq.com>
2019-11-20 00:24:10 +0800

    auto_control.py

---
+++ control_manual.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-  
+"""
+@version : 
+@file : control.py
+@time : 2019/04/16 00:09:31
+@func : 
+"""
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime
+import subprocess
+import os
+import shutil
+import time
+def copydir(path, out):
+    for files in os.listdir(path):
+        name = os.path.join(path, files)
+        back_name = os.path.join(out, files)
+        if os.path.isfile(name):
+            if os.path.isfile(back_name):
+                # print(back_name)
+                pass
+            else:
+                # print(back_name)
+                pass
+        else:
+            if not os.path.isdir(back_name):
+                os.makedirs(back_name)
+                copydir(name, back_name)
+
+def copyfile(src, dsc):
+    tmp_list=[]
+    for root, dirs, files in os.walk(src):
+        if dirs:
+            dsc_path=dsc
+        else:
+            dsc_path=os.path.join(dsc, os.path.split(root)[1])
+        for file in files:
+            src_file=os.path.join(root,file)
+            tmp_trup=(src_file,dsc_path)
+            tmp_list.append(tmp_trup)
+    return tmp_list
+
+def cut_single_file():
+    A = r"/root/homework"
+    B = r"/root/Projects/gitdemo/test/Tool/Tool"
+
+    # A=r"D:\test\a"
+    # B=r"D:\test\b"
+    copydir(A, B)
+    result = copyfile(A, B)
+    if result:
+        src_file, dsc_path = result.pop()
+        print(src_file)
+        shutil.move(src_file, dsc_path)
+
+#本类的方法
+def push():
+    print('Tick! The time is: %s' % datetime.now())
+    cmd = 'sh push.sh auto_control.py'
+    subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+
+if __name__ == '__main__':
+    
+    cut_single_file()
+    time.sleep(1)
+    push()
